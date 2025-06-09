627ea35e80831ebbeda007685017ea16226085f6
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-09 17:48:08 +0700

    feat: import modul utils

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-basic/utils"
+)
 
-func area(radius float64) float64 {
-	return 22 / 7 * radius * radius
-}
 
-func circumference(radius float64) float64 {
-	return 2 * 22 / 7 * radius
-}
 
 func main() {
-	fmt.Println(area(7))
-	fmt.Println(circumference(7))
+	fmt.Println(utils.Area(7))
+	fmt.Println(utils.Circumference(7))
 }
